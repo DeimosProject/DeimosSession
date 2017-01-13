d721430cbb3242803ad9a1080a82e7cfef613c28
rez1dent3 <maksim.babichev95@gmail.com>
2017-01-13 14:18:13 +0300

    add docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Deimos\\Session\\Extension"],["c","Deimos\\Session\\Extensions\\Flash"],["c","Deimos\\Session\\Extensions\\Variable"],["c","Deimos\\Session\\Session"],["c","Exception"],["c","InvalidArgumentException"],["c","LogicException"],["c","Throwable"]];
